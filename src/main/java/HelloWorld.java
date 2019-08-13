a32d81920fc3bd143413f9da09763a24027a0a0b
dmytrotsurikov <dtsurikov@hotmail.com>
2019-08-13 17:30:07 +0300

    first commit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+       // int a = 1;
+       // int b = 2 ;
+       // System.out.println( a  +  " Hello world! " + b );
+      //   System.out.printf("%d плюс %d  ",  a,b) ;
+        Scanner scanner= new  Scanner(System.in) ;
+        System.out.println("Enter x:");
+        int x = scanner.nextInt();
+        System.out.println("Enter y:");
+        int y = scanner.nextInt();
+        System.out.printf("%d * %d = %d", x, y,  x * y );
+    }
+
+}
+` `
